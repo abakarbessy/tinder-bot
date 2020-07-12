375471aa0f76a47a9418ce87039b8cd1d85cfccd
Hendrik Sutrisna <hendrik.sutrisna@growthops.asia>
2020-07-12 23:16:24 +0800

    download_image & save_biodata function

--- tinder-automator.py
+++ tinder-automator.py
@@ -2,9 +2,11 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 from PIL import Image
+from secrets import username, password
 
 import os
 import random
+import requests
 
 class TinderAutomator():
   def __init__(self): 
@@ -18,13 +20,14 @@ class TinderAutomator():
     login_fb = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
     login_fb.click()
 
+    # switch to login popup
     base_window = self.driver.window_handles[0]
     self.driver.switch_to.window(self.driver.window_handles[1])
 
     login_input = self.driver.find_element_by_xpath('//*[@id="email"]')
     password_input = self.driver.find_element_by_xpath('//*[@id="pass"]')
-    login_input.send_keys('<your-fb-username>')
-    password_input.send_keys('<your-fb-password')
+    login_input.send_keys(username)
+    password_input.send_keys(password)
 
     login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
     login_btn.click()
@@ -100,13 +103,47 @@ class TinderAutomator():
     self.driver.find_element_by_tag_name('body').send_keys(Keys.ESCAPE)
     sleep(0.5)
 
+  def download_image(self, name, idx, path):
+
+    sleep(1)
+    sequence = idx + 1
+    web_element = """//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/div/div[""" + str(sequence) + """]/div/div"""
+    style_string = self.driver.find_element_by_xpath(web_element).get_attribute("style")
+    
+    # parse style element
+    style_string = style_string.split(' url("')[1].replace('");', '')
+    image_url = style_string.split(' ')[0]
+    # print(image_url)
+    
+    # download image, convert as jpg
+    img_data = requests.get(image_url).content
+    with open(path + '/' + name + '_' + str(sequence) + '.jpg', 'wb') as handler:
+      handler.write(img_data)
+
+
+  def save_biodata(self):
+    name = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[6]/div/div[1]/div/div/span').text
+    age = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[6]/div/div[1]/div/span').text
+
+    path = "pics/"+name+"-"+age
+    os.mkdir(path)
+    number_of_photos = len(self.driver.find_elements_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/div/*'))
+    print(number_of_photos)
+
+    for idx in range (number_of_photos):
+      self.download_image(name, idx, path)
+      # next photo
+      self.driver.find_element_by_tag_name('body').send_keys(Keys.SPACE)
+
+
   def auto_swipe(self):
     while True:
-      sleep(1)
-      self.show_profile()
-      self.hide_profile()
-      self.save_card_to_photo()
-      self.next_photo()
+      sleep(2)
+      # self.show_profile()
+      # self.hide_profile()
+      # self.save_card_to_photo()
+      # self.next_photo()
+      self.save_biodata()
       try:
         if random.randint(0,99) > 10:  
           self.dislike()
@@ -120,6 +157,7 @@ class TinderAutomator():
     self.login_fb()
     self.clear_notification()
     self.auto_swipe()
+
 
 bot = TinderAutomator()
 bot.run()
